78cb07b3ae9d069803095a45f43795d07f983ce2
Bryan Stovall <ausbt@asu.edu>
2026-03-22 02:08:18 -0700

    To complete step 4.

--- loggerMiddleware.js
+++ loggerMiddleware.js
@@ -1,4 +1,5 @@
 // loggerMiddleware.js
+// This is a common Logger (ausbt@asu.edu - 03/22/26)
 function loggerMiddleware(req, res, next) {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
